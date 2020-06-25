37c33b7a509504af516157f7126979a3efd6d91a
benbryson789 <deborashelby@gmail.com>
2020-06-25 00:17:47 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace GrandCircusEncodedMessages
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string message = "";
+            int checksum = 0;
+
+            Console.WriteLine("What is your message?");
+
+            string input  = Console.ReadLine().ToUpper();
+
+            foreach (char character in input)
+            {
+                // A = 65 in Unicode
+                message = message + $"{character - 64}-";
+                checksum = checksum + character;
+            }
+
+            Console.WriteLine("Your encoded message is " + message.TrimEnd('-'));
+            Console.WriteLine("Message checksum is " + checksum);
+
+            // Just for sake of pausing  
+            Console.Read();
+        }
+    }
+}
